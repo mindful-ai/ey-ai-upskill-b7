4fbf61820cb40d31506f92537eee31896a2f5f8a
mindful-ai <mindfullearningindia@outlook.com>
2026-06-10 12:08:25 +0530

    Add files via upload

---
+++ 16-rag-capstone/README.md
@@ -0,0 +1,14 @@
+Quick Run:
+cd cis_policy_agent
+
+source venv/bin/activate  # or Windows equivalent
+
+---------------------------------------------------
+python ingestion/ingest.py
+
+Understand retrieval.py
+python retrieval/test_retrieval.py
+
+
+Understand policy_assistant.py
+python main.py
---
+++ 16-rag-capstone/cis-basic/retrieval/retriever.py
@@ -0,0 +1,25 @@
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+keypath = r"E:\Lenovo Ideapad 330\company-material\digital-workforce-transformation\ai-upskill-6\key-vault\openai\temp.key"
+with open(keypath) as f:
+    api_key = f.read().strip()
+
+def get_retriever():
+    embeddings = OpenAIEmbeddings(api_key=api_key)
+
+    db = FAISS.load_local(
+        "vectorstore/",
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+    retriever = db.as_retriever(
+        search_kwargs={"k": 20}  # top 4 chunks
+    )
+
+    return retriever
