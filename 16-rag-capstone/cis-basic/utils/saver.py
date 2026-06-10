ee7327d7156b74b52fb69fdc93c0d829f9feb302
mindful-ai <mindfullearningindia@outlook.com>
2026-06-10 15:53:22 +0530

    Delete 16-rag-capstone directory

--- 16-rag-capstone/cis-basic/utils/saver.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def save_response(query, response):
-    filename = "outputs.txt"
-
-    with open(filename, "a") as f:
-        f.write(f"\n--- {datetime.now()} ---\n")
-        f.write(f"Query: {query}\n")
-        f.write(f"Response:\n{response}\n")
